d60b6d9a3dbf00caeefc5aacf3e154cdfef0a494
Chrysyeh <chrys1129@gmail.com>
2022-05-18 18:44:06 +0800

    change port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // require packages used in the project
 const express = require('express')
 const app = express()
-const port = 2000
+const port = 3000
 const exphbs = require('express-handlebars')
 const res = require('express/lib/response')
 const restaurantsData = require('./restaurant.json').results
